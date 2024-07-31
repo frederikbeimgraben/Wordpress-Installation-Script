b682d5865974c622909c97366dd6db0a44c4cf1e
Frederik Beimgraben <frederik@beimgraben.net>
2024-07-31 17:40:34 +0200

    Add Makefile to compile to bin; Add LICENSE to .py header

---
+++ Makefile
@@ -0,0 +1,34 @@
+# Compile the python script into an executable using Cython
+
+TARGET_FILE=make_wp_compose_project.py
+TARGET_C_FILE=build/$(patsubst %.py,%.c,$(TARGET_FILE))
+TARGET_O_FILE=build/$(patsubst %.py,%,$(TARGET_FILE))
+
+PYTHONLIBVER=python$(shell python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')$(shell python3-config --abiflags)
+
+all: create_build_dir test_cython_installed compile_cython compile_c
+
+create_build_dir:
+	@echo "Creating build directory..."
+	@mkdir -p build
+	@echo "Build directory created."
+
+test_cython_installed:
+	@echo "Checking if Cython is installed..."
+	@python3 -c "import Cython" || (echo "Cython is not installed. Please install it using 'pip install Cython'." && exit 1)
+	@echo "Cython is installed."
+
+compile_cython:
+	@echo "Compiling the python script into an executable using Cython..."
+	@cython -3 --embed $(TARGET_FILE) -o $(TARGET_C_FILE)
+	@echo "Compilation successful."
+
+compile_c:
+	@echo "Compiling the generated C file: $(TARGET_C_FILE) into an executable..."
+	@gcc -o $(TARGET_O_FILE) $(TARGET_C_FILE) -I/usr/include/$(PYTHONLIBVER) -l$(PYTHONLIBVER) -lpthread -lm -lutil -ldl
+	@echo "Compilation successful."
+
+clean:
+	@echo "Cleaning up..."
+	@rm -f $(TARGET_FILE:.py=.c)
+	@echo "Cleanup successful."
--- make_wp_compose_project.py
+++ make_wp_compose_project.py
@@ -1,7 +1,75 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Create a docker compose project for a wordpress site running behind a reverse proxy
+# =============================================================================
+# Copyright (C) 2024 Frederik Beimgraben
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# =============================================================================
+
+Create a docker compose project for a wordpress site running behind a nginx
+reverse proxy
+
+Usage of the script:
+    python3 make_wp_compose_project.py [ { -I [ -C ] | -U } ] [ -n <hostname> ] [ -p <port> ] [ -m <mount_folder> ] [ -d <db_passwd> ] [ -r <db_passwd_root> ]
+
+    [no action options]:
+        Create configuration files for a new project in the current directory
+
+    -I: Install the project
+    -C: Start Certbot to get a certificate for the domain
+    -U: Uninstall the project
+    -c: Clean the project (remove all files)
+
+    -n: The hostname of the site (default: localhost)
+    -p: The port of the site (default: 8080)
+    -m: The folder to mount the database data (default: db_data)
+    -d: The password for the database user (default: <random>)
+    -r: The password for the database root user (default: <random>)
+
+    -h: Show this help message
+
+    The script will create a docker-compose.yml file and a .env file in the
+    current directory. The .env file will contain the passwords for the database
+    users as well as the hostname and port of the site. The docker-compose.yml
+    file will contain the configuration for the wordpress site and the nginx
+    reverse proxy. The script will also create a folder for the database data
+    of the wordpress site.
+
+    If the -I option is used, the script will:
+        - Create the configuration files
+        - Start the containers
+        - Install the nginx reverse proxy configuration
+
+    If the -C option is used, the script will:
+        - Start Certbot to get a certificate for the domain
+
+    If the -U option is used, the script will:
+        - Stop the containers
+        - Remove the containers
+        - Uninstall the nginx reverse proxy configuration
+
+    If the -c option is used, the script will:
+        - Clean the project (remove all files)
+
+    The script will check if the required programs are installed. Furthermore
+    it will check if the DNS-Configuration is correct for the setup to work
+    when requesting a certificate with Certbot.
+
+# =============================================================================
+# Have fun using the script! But be careful, it might overwrite existing files!
+# =============================================================================
 """
 
 from enum import Enum
@@ -218,8 +286,8 @@ class Checks:
                     break
                 except socket.gaierror:
                     if not latch:
-                        return False, f'Waiting for DNS to resolve for {hostname}. Press CTRL+C to proceed without waiting...'
                         latch = True
+                        print_log_fancy(Level.INFO, 'Waiting for DNS to resolve for {hostname}. Press CTRL+C to proceed without waiting...')
                     time.sleep(1)
             except KeyboardInterrupt:
                 return True, 'Continuing without waiting for DNS to resolve'
