80f52c557e47bfc2186b519d82ff170913110d21
Frederik Beimgraben <frederik@beimgraben.net>
2024-07-31 21:55:27 +0200

    Simplify header

--- wordpress_install.py
+++ wordpress_install.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
+# =============================================================================
+# author:  Frederik Beimgraben
+# email:   frederik@beimgraben.net
+# date:    2024-07-31
+# license: GPL-3.0
+# version: 1.0.0
 # =============================================================================
 # Copyright (C) 2024 Frederik Beimgraben
 #
@@ -17,64 +22,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # =============================================================================
-
-Create a docker compose project for a wordpress site running behind a nginx
-reverse proxy
-
-Usage of the script:
-    python3 wordpress_install.py [ { -I [ -C ] | -U } ] [ -n <hostname> ] [ -p <port> ] [ -m <mount_folder> ] [ -d <db_passwd> ] [ -r <db_passwd_root> ]
-
-    [no action options]:
-        Create configuration files for a new project in the current directory
-
-    -I: Install the project
-    -C: Start Certbot to get a certificate for the domain
-    -U: Uninstall the project
-
-    -c: Clean the project (remove all files)
-
-    -i: Interactive mode (ask for the following options)
-    -s: Silent mode (don't show prompts)
-
-    -n: The hostname of the site (default: localhost)
-    -p: The port of the site (default: 8080)
-    -m: The folder to mount the database data (default: db_data)
-    -d: The password for the database user (default: <random>)
-    -r: The password for the database root user (default: <random>)
-
-    -h: Show this help message
-
-    The script will create a docker-compose.yml file and a .env file in the
-    current directory. The .env file will contain the passwords for the database
-    users as well as the hostname and port of the site. The docker-compose.yml
-    file will contain the configuration for the wordpress site and the nginx
-    reverse proxy. The script will also create a folder for the database data
-    of the wordpress site.
-
-    If the -I option is used, the script will:
-        - Create the configuration files
-        - Start the containers
-        - Install the nginx reverse proxy configuration
-
-    If the -C option is used, the script will:
-        - Start Certbot to get a certificate for the domain
-
-    If the -U option is used, the script will:
-        - Stop the containers
-        - Remove the containers
-        - Uninstall the nginx reverse proxy configuration
-
-    If the -c option is used, the script will:
-        - Clean the project (remove all files)
-
-    The script will check if the required programs are installed. Furthermore
-    it will check if the DNS-Configuration is correct for the setup to work
-    when requesting a certificate with Certbot.
-
-# =============================================================================
 # Have fun using the script! But be careful, it might overwrite existing files!
 # =============================================================================
-"""
 
 from enum import Enum
 import os
